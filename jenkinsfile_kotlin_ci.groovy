f4cdc7ca70efd3e7fe2f6e9da8d5f7cd6d916b43
ameeentos <albertzhen1115@gmail.com>
2020-12-07 21:14:57 +0300

    Add files via upload

---
+++ jenkinsfile_kotlin_ci.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent none
+    stages {
+        stage('Build') {
+            agent any
+            steps{
+                checkout scm
+                withGradle{
+                    sh('./gradle build')
+                }
+                sh("chmod 775 /var/lib/jenkins/workspace/app_builder/gradlew")
+            }
+        }
+        post {
+            always {
+                junit '**/target/*.xml'
+                archiveArtifacts artifacts: 'app/build/outputs/apk/debug/*.apk', followSymlinks: false, onlyIfSuccessful: true
+            }
+        }
+    }
+}
